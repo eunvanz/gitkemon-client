598bf041038144cd2d7eaee7bcba037473319242
Benjamin <eunvanz@naver.com>
2021-07-27 01:31:41 +0900

    feat: RecoilRoot 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,14 @@ import "../styles/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />;
+    </RecoilRoot>
+  );
 }
 export default MyApp;

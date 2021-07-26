81987ba4144b801541de38c93341f44230eb5ff2
Benjamin <eunvanz@naver.com>
2021-07-27 01:07:37 +0900

    feat: 사이드바 하위메뉴

--- components/BaseLayout/BaseLayout.tsx
+++ components/BaseLayout/BaseLayout.tsx
@@ -1,5 +1,5 @@
-import { Fragment, ReactNode, useState } from "react";
-import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, ReactNode, SVGProps, useState } from "react";
+import { Dialog, Disclosure, Transition } from "@headlessui/react";
 import {
   BellIcon,
   GlobeIcon,
@@ -18,25 +18,45 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import DropDownMenu from "../DropDownMenu";
 import { useRouter } from "next/router";
 
-const navigation = [
-  { name: "Home", href: ROUTES.HOME, icon: HomeIcon, current: true },
+interface NavigationItem {
+  name: string;
+  href?: string;
+  icon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  children?: {
+    name: string;
+    href: string;
+  }[];
+}
+
+const navigation: NavigationItem[] = [
+  { name: "Home", href: ROUTES.HOME, icon: HomeIcon },
   {
     name: "Pokemon Hunt",
     href: ROUTES.HUNT,
     icon: GlobeIcon,
-    current: false,
   },
   {
     name: "Ranking",
-    href: ROUTES.RANKING,
     icon: SortAscendingIcon,
-    current: false,
+    children: [
+      {
+        name: "Collection Ranking",
+        href: `${ROUTES.RANKING}?tab=collection`,
+      },
+      {
+        name: "Pokemon Ranking",
+        href: `${ROUTES.RANKING}?tab=pokemon`,
+      },
+      {
+        name: "Contribution Ranking",
+        href: `${ROUTES.RANKING}?tab=contribution`,
+      },
+    ],
   },
   {
     name: "Pokemon Workshop",
     href: ROUTES.WORKSHOP,
     icon: PhotographIcon,
-    current: false,
   },
 ];
 
@@ -115,24 +135,10 @@ const BaseLayout = ({ children, user }: BaseLayoutProps) => {
               </div>
               <div className="mt-5 flex-1 h-0 overflow-y-auto">
                 <nav className="px-2 space-y-1">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className={classNames(
-                        item.current
-                          ? "bg-blue-700 text-white"
-                          : "text-blue-100 hover:bg-blue-600",
-                        "group flex items-center px-2 py-2 text-base font-medium rounded-md"
-                      )}
-                    >
-                      <item.icon
-                        className="mr-4 flex-shrink-0 h-6 w-6 text-blue-200"
-                        aria-hidden="true"
-                      />
-                      {item.name}
-                    </a>
-                  ))}
+                  <SidebarItems
+                    items={navigation}
+                    onClickItem={() => setSidebarOpen(false)}
+                  />
                 </nav>
               </div>
             </div>
@@ -158,24 +164,7 @@ const BaseLayout = ({ children, user }: BaseLayoutProps) => {
             </div>
             <div className="mt-5 flex-1 flex flex-col">
               <nav className="flex-1 px-2 space-y-1">
-                {navigation.map((item) => (
-                  <Link href={item.href} key={item.name}>
-                    <a
-                      className={classNames(
-                        router.asPath === item.href
-                          ? "bg-blue-700 text-white"
-                          : "text-blue-100 hover:bg-blue-600",
-                        "group flex items-center px-2 py-2 text-sm font-medium rounded-md"
-                      )}
-                    >
-                      <item.icon
-                        className="mr-3 flex-shrink-0 h-6 w-6 text-blue-200"
-                        aria-hidden="true"
-                      />
-                      {item.name}
-                    </a>
-                  </Link>
-                ))}
+                <SidebarItems items={navigation} />
               </nav>
             </div>
           </div>
@@ -265,6 +254,96 @@ const BaseLayout = ({ children, user }: BaseLayoutProps) => {
         </main>
       </div>
     </div>
+  );
+};
+
+interface SidebarItemsProps {
+  items: NavigationItem[];
+  onClickItem?: VoidFunction;
+}
+
+const SidebarItems = ({ items, onClickItem }: SidebarItemsProps) => {
+  const router = useRouter();
+
+  return (
+    <>
+      {items.map((item) =>
+        !item.children ? (
+          <Link href={item.href!} key={item.name}>
+            <a
+              className={classNames(
+                router.asPath === item.href
+                  ? "bg-blue-700 text-white"
+                  : "text-blue-100 hover:bg-blue-600",
+                "group flex items-center px-2 py-2 text-sm font-medium rounded-md"
+              )}
+              onClick={onClickItem}
+            >
+              <item.icon
+                className="mr-3 flex-shrink-0 h-6 w-6 text-blue-200"
+                aria-hidden="true"
+              />
+              {item.name}
+            </a>
+          </Link>
+        ) : (
+          <Disclosure
+            as="div"
+            defaultOpen={item.children
+              .map((child) => child.href)
+              .includes(router.asPath)}
+            key={item.name}
+            className="space-y-1"
+          >
+            {({ open }) => (
+              <>
+                <Disclosure.Button
+                  className={classNames(
+                    router.asPath === item.href
+                      ? "bg-blue-700 text-white"
+                      : "text-blue-100 hover:bg-blue-600",
+                    "group w-full flex items-center pl-2 pr-1 py-2 text-left text-sm font-medium rounded-md"
+                  )}
+                >
+                  <item.icon
+                    className="mr-3 flex-shrink-0 h-6 w-6 text-blue-200"
+                    aria-hidden="true"
+                  />
+                  <span className="flex-1">{item.name}</span>
+                  <svg
+                    className={classNames(
+                      open ? "text-blue-100 rotate-90" : "text-blue-100",
+                      "ml-3 flex-shrink-0 h-5 w-5 transform"
+                    )}
+                    viewBox="0 0 20 20"
+                    aria-hidden="true"
+                  >
+                    <path d="M6 6L14 10L6 14V6Z" fill="currentColor" />
+                  </svg>
+                </Disclosure.Button>
+                <Disclosure.Panel className="space-y-1">
+                  {item.children!.map((subItem) => (
+                    <Link href={subItem.href} key={subItem.name}>
+                      <a
+                        className={classNames(
+                          router.asPath === subItem.href
+                            ? "bg-blue-700 text-white"
+                            : "text-blue-100 hover:bg-blue-600",
+                          "group flex items-center px-2 py-2 text-sm font-medium rounded-md pl-11"
+                        )}
+                        onClick={onClickItem}
+                      >
+                        {subItem.name}
+                      </a>
+                    </Link>
+                  ))}
+                </Disclosure.Panel>
+              </>
+            )}
+          </Disclosure>
+        )
+      )}
+    </>
   );
 };
 

f491a4308f7beb5e22521f7d070a0e9e1671c725
Benjamin <eunvanz@naver.com>
2021-07-28 01:40:38 +0900

    feat: withBaseLayout hoc 작성

--- hocs/withAuth.tsx
+++ hocs/withAuth.tsx
@@ -1,23 +1,38 @@
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
-import { useSetRecoilState } from "recoil";
+import React, { useCallback, useEffect } from "react";
+import { useRecoilState } from "recoil";
+import api from "../api";
 import { userState } from "../state/user";
 import { User } from "../types";
 
 /**
- * withAuthServerSideProps와 같이 쓰여야 함
+ * user를 전달 받아서 전역 상태에 세팅
+ * user가 없을 경우 토큰으로 로그인 수행
+ * 서버사이드 렌더링 시 withAuthServerSideProps와 같이 쓰여야 함
  * @param WrappedComponent
  * @returns
  */
 const withAuth = (WrappedComponent: React.FC<any>) => {
-  const Wrapper = ({ user, data }: { user: User; data: any }) => {
-    const setUser = useSetRecoilState(userState);
+  const Wrapper = ({ user: userProp, data }: { user: User; data: any }) => {
+    const [user, setUser] = useRecoilState(userState);
 
     const router = useRouter();
 
+    const login = useCallback(async () => {
+      const loginUser = await api.loginWithToken();
+      loginUser && setUser(loginUser);
+    }, [setUser]);
+
     useEffect(() => {
-      setUser(user);
-    }, [router, setUser, user]);
+      if (user) {
+        return;
+      }
+      if (userProp) {
+        setUser(user);
+      } else {
+        login();
+      }
+    }, [login, router, setUser, user, userProp]);
 
     return <WrappedComponent {...data.props} />;
   };
---
+++ hocs/withBaseLayout.tsx
@@ -0,0 +1,23 @@
+import BaseLayoutContainer from "../components/BaseLayout";
+import compose from "compose-function";
+import withAuth from "./withAuth";
+
+/**
+ * BaseLayoutContainer로 래핑된 컴포넌트를 리턴
+ * 서버사이드 렌더링 시 withAuthServerSideProps 와 함께 쓰여야 함
+ * @param WrappedComponent
+ * @returns BaseLayoutContainer로 래핑된 컴포넌트
+ */
+const withBaseLayout = (WrappedComponent: React.FC<any>) => {
+  const Wrapper = (props: any) => {
+    return (
+      <BaseLayoutContainer user={props.user}>
+        <WrappedComponent {...props} />
+      </BaseLayoutContainer>
+    );
+  };
+
+  return Wrapper;
+};
+
+export default compose(withAuth, withBaseLayout);

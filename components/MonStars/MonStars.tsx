faa498612a17cacfae93840dbd4e933585bc0f5b
Benjamin <eunvanz@naver.com>
2021-07-30 09:30:03 +0900

    feat: MonStars 타입 상속

--- components/MonStars/MonStars.tsx
+++ components/MonStars/MonStars.tsx
@@ -1,12 +1,13 @@
 import { useMemo } from "react";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon, FontAwesomeIconProps } from "@fortawesome/react-fontawesome";
+import cx from "classnames";
 
-export interface MonStarsProps {
+export interface MonStarsProps extends Omit<FontAwesomeIconProps, "icon"> {
   stars: number;
 }
 
-const MonStars: React.FC<MonStarsProps> = ({ stars }) => {
+const MonStars: React.FC<MonStarsProps> = ({ stars, className, ...restProps }) => {
   const { emptyColor, fillColor } = useMemo(() => {
     if (stars > 5) {
       return { emptyColor: "text-gray-500", fillColor: "text-yellow-400" };
@@ -20,10 +21,12 @@ const MonStars: React.FC<MonStarsProps> = ({ stars }) => {
       {Array.from({ length: 5 }).map((_, index) => (
         <FontAwesomeIcon
           key={index}
-          className={
-            stars % 5 === 0 || (stars % 5) - index - 1 >= 0 ? fillColor : emptyColor
-          }
+          className={cx(
+            stars % 5 === 0 || (stars % 5) - index - 1 >= 0 ? fillColor : emptyColor,
+            className,
+          )}
           icon={faStar}
+          {...restProps}
         />
       ))}
     </>

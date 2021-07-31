abf0547bceac954f85f69883d1f0c31fed727df3
Benjamin <eunvanz@naver.com>
2021-08-01 02:43:25 +0900

    feat: ExchangeCode 페이지에 애니메이션 추가

--- pages/exchange-code/ExchangeCode.view.tsx
+++ pages/exchange-code/ExchangeCode.view.tsx
@@ -2,7 +2,7 @@ const ExchangeCode = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-white py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8 text-center text-3xl text-gray-600 font-extrabold">
-        <h3>Connecting with Github...</h3>
+        <h3 className="animate-pulse">Connecting with Github...</h3>
       </div>
     </div>
   );

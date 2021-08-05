8f7ab7939ef5ceb1ce5194a0b4d0cd9671404a7a
Benjamin <eunvanz@naver.com>
2021-08-06 00:33:04 +0900

    Revert "chore: 스토리북 업그레이드"
    This reverts commit b5a14c35ad7600a926021f899a18a85c19ec4fc0.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,21 +3,5 @@ module.exports = ({ config }) => {
     "next/image": require.resolve("./__mocks__/NextJSImageMock.js"),
   };
 
-  config.module.rules.push({
-    test: /\.less$/,
-    use: [
-      'style-loader',
-      'css-loader',
-      {
-        loader: 'less-loader',
-        options: {
-          lessOptions: {
-            javascriptEnabled: true,
-          }
-        },
-      },
-    ],
-  })
-
   return config;
 };

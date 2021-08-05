b5a14c35ad7600a926021f899a18a85c19ec4fc0
Benjamin <eunvanz@naver.com>
2021-08-05 23:46:45 +0900

    chore: 스토리북 업그레이드

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,10 @@
-const path = require("path");
-
 module.exports = {
   stories: ["../**/*.stories.mdx", "../**/*.stories.tsx"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-ant-design", "storybook-preset-less"],
-  // webpackFinal: async (config) => {
-  //   config.module.rules.push({
-  //     test: /\.less$/i,
-  //     loader: [
-  //       "style-loader",
-  //       "css-loader",
-  //       {
-  //         loader: "less-loader",
-  //         options: { javascriptEnabled: true }
-  //       }
-  //     ],
-  //     include: path.resolve(__dirname, "../")
-  //   });
-  //   return config;
-  // }
+  addons: [
+    "@storybook/addon-links", 
+    "@storybook/addon-essentials", 
+  ],
+  core: {
+    builder: "webpack5"
+  }
 };

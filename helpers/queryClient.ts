65c03d435887d97e70ee9b6b7a844e4dfa6c036a
Benjamin <eunvanz@naver.com>
2021-08-07 00:30:27 +0900

    feat: queryClient 세팅

---
+++ helpers/queryClient.ts
@@ -0,0 +1,7 @@
+import { QueryClient } from "react-query";
+
+const queryClient = new QueryClient();
+
+// TODO: 공통로직 세팅
+
+export default queryClient;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import { config } from "@fortawesome/fontawesome-svg-core";
 import type { AppProps } from "next/app";
+import { QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { RecoilRoot } from "recoil";
+import queryClient from "../helpers/queryClient";
 import "../styles/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
@@ -8,9 +11,12 @@ config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <Component {...pageProps} />;
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <Component {...pageProps} />;
+      </RecoilRoot>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 export default MyApp;

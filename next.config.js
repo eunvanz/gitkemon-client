00ac0ec26a40711e099969e160e0a706c7cf55d9
Benjamin <eunvanz@naver.com>
2021-08-06 23:35:32 +0900

    chore: withAntdLess 제거

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require("next/constants");
-const withAntdLess = require('next-plugin-antd-less')
 
-module.exports = withAntdLess((phase) => {
+module.exports = (phase) => {
   // when started in development mode `next dev` or `npm run dev` regardless of the value of STAGING environmental variable
   const isDev = phase === PHASE_DEVELOPMENT_SERVER;
   // when `next build` or `npm run build` is used
@@ -20,4 +19,4 @@ module.exports = withAntdLess((phase) => {
       domains: ["tailwindui.com", "images.unsplash.com", "avatars.githubusercontent.com"],
     },
   };
-});
+};

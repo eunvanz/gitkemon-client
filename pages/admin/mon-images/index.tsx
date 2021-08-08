3322ae1e67403eb36c17dd371bfa4fe69d2659e1
Benjamin <eunvanz@naver.com>
2021-08-08 13:36:46 +0900

    feat: MonImagesPage 작성

---
+++ pages/admin/mon-images/index.tsx
@@ -0,0 +1,16 @@
+import withAdminBaseLayout from "../../../hocs/withAdminBaseLayout";
+import withAuthServerSideProps from "../../../hocs/withAuthServerSideProps";
+import MonImages from "./MonImages.view";
+import useMonImagesProps from "./useMonImagesProps";
+
+const MonImagesPage: React.FC<void> = () => {
+  const props = useMonImagesProps();
+
+  return <MonImages {...props} />;
+};
+
+export const getServerSideProps = withAuthServerSideProps({
+  isAuthRequired: true,
+})();
+
+export default withAdminBaseLayout(MonImagesPage);

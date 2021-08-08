e27e4e683f2250a2e44908cd2a14f35c45f8f9e4
Benjamin <eunvanz@naver.com>
2021-08-08 11:32:25 +0900

    feat: useUserQuery에 cacheTime, staleTime 설정

--- queries/useUserQuery.ts
+++ queries/useUserQuery.ts
@@ -4,6 +4,8 @@ import { QUERY_KEY, User } from "../types";
 
 const useUserQuery = (queryOptions?: UseQueryOptions<User | null>) => {
   const query = useQuery<User | null>(QUERY_KEY.USER, () => api.loginWithToken(), {
+    cacheTime: Infinity,
+    staleTime: Infinity,
     ...queryOptions,
   });
   return query;

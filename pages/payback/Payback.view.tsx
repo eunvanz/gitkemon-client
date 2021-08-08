29758f8cf2809c2febc9a4c95b149fa85730afd2
Benjamin <eunvanz@naver.com>
2021-08-09 04:56:40 +0900

    style: 보상 페이지 스타일 수정

--- pages/payback/Payback.view.tsx
+++ pages/payback/Payback.view.tsx
@@ -25,7 +25,7 @@ const Payback: React.FC<PaybackProps> = ({
 }) => {
   if (!paybackResult) {
     return !isLoading ? (
-      <div className="min-h-screen flex items-center justify-center bg-white py-12 px-4 sm:px-6 lg:px-8">
+      <div className="min-h-full flex items-center justify-center bg-white py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
             <h2 className="mt-6 text-center text-xl text-gray-600">

13cd52d1cc5b7db5787aafd9bf0f46ab068ce6f0
Benjamin <eunvanz@naver.com>
2021-08-09 04:12:14 +0900

    feat: donation 서버사이드 prop cnrk

--- pages/donation/index.tsx
+++ pages/donation/index.tsx
@@ -1,3 +1,4 @@
+import withAuthServerSideProps from "../../hocs/withAuthServerSideProps";
 import withBaseLayout from "../../hocs/withBaseLayout";
 import Donation from "./Donation.view";
 import useDonationProps from "./useDonationProps";
@@ -7,5 +8,9 @@ const DonationPage: React.FC<void> = () => {
 
   return <Donation {...props} />;
 };
+
+export const getServerSideProps = withAuthServerSideProps({
+  isAuthRequired: true,
+})();
 
 export default withBaseLayout(DonationPage);

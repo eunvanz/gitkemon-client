253ae273fc7eefeec7ef24960fb20a372db855af
Benjamin <eunvanz@naver.com>
2021-08-08 12:19:51 +0900

    feat: MonImages.view 작성

---
+++ pages/admin/mon-images/MonImages.stories.tsx
@@ -0,0 +1,16 @@
+import type { ComponentStory, ComponentMeta } from "@storybook/react";
+import mockMonImage from "../../../api/mocks/monImage";
+import { createStoryComponent } from "../../../helpers/storybookHelpers";
+import MonImages from "./MonImages.view";
+
+export default {
+  title: "pages/admin/몬스터 이미지 목록",
+  component: MonImages,
+  args: {
+    monImages: mockMonImage.monImages,
+  },
+} as ComponentMeta<typeof MonImages>;
+
+const Template: ComponentStory<typeof MonImages> = (args) => <MonImages {...args} />;
+
+export const 기본 = createStoryComponent(Template);
--- pages/admin/mon-images/MonImages.view.tsx
+++ pages/admin/mon-images/MonImages.view.tsx
@@ -1,11 +1,96 @@
+import { useCallback, useMemo } from "react";
+import { Button, Modal, Table } from "antd";
+import Image from "next/image";
 import { MonImage } from "../../../types";
+import styles from "./MonImages.module.css";
 
 export interface MonImagesProps {
   monImages: MonImage[];
+  onEdit: (id: number) => void;
+  onDelete: (id: number) => void;
+  onCreate: VoidFunction;
 }
 
-const MonImages: React.FC<MonImagesProps> = ({ monImages }) => {
-  return <></>;
+const MonImages: React.FC<MonImagesProps> = ({
+  monImages,
+  onEdit,
+  onDelete,
+  onCreate,
+}) => {
+  const onViewImage = useCallback((imageUrl: string) => {
+    Modal.info({
+      content: <Image src={imageUrl} alt="Mon image" layout="fill" />,
+    });
+  }, []);
+
+  const dataSource = useMemo(() => {
+    return monImages.map((monImage) => ({
+      key: monImage.id,
+      id: monImage.id,
+      monName: monImage.__mon__!.name,
+      designerName: monImage.designerName,
+      imageUrl: monImage.imageUrl,
+    }));
+  }, [monImages]);
+
+  const columns = useMemo(() => {
+    return [
+      {
+        title: "ID",
+        dataIndex: "id",
+        key: "id",
+      },
+      {
+        title: "Mon name",
+        dataIndex: "monName",
+        key: "monName",
+      },
+      {
+        title: "Designer name",
+        dataIndex: "designerName",
+        key: "designerName",
+      },
+      {
+        title: "View image",
+        dataIndex: "imageUrl",
+        key: "imageUrl",
+        render: (imageUrl: string) => (
+          <Button type="link" onClick={() => onViewImage(imageUrl)}>
+            View
+          </Button>
+        ),
+      },
+      {
+        title: "Edit",
+        dataIndex: "id",
+        key: "Edit",
+        render: (id: number) => (
+          <Button type="link" onClick={() => onEdit(id)}>
+            Edit
+          </Button>
+        ),
+      },
+      {
+        title: "Delete",
+        dataIndex: "id",
+        key: "Delete",
+        render: (id: number) => (
+          <Button type="link" onClick={() => onDelete(id)}>
+            Delete
+          </Button>
+        ),
+      },
+    ];
+  }, [onDelete, onEdit, onViewImage]);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.action}>
+        <Button onClick={onCreate}>Create</Button>
+      </div>
+      <Table dataSource={dataSource} columns={columns} />
+    </div>
+  );
 };
 
 export default MonImages;

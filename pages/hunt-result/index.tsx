17739a9e063daf80f835959a24c6cfd941d3a214
Benjamin <eunvanz@naver.com>
2021-08-15 17:04:52 +0900

    feat: 포켓몬 채집 결과 페이지에 리다이렉트 로직적용

--- pages/hunt-result/index.tsx
+++ pages/hunt-result/index.tsx
@@ -1,4 +1,5 @@
 import withBaseLayout from "../../hocs/withBaseLayout";
+import ROUTES from "../../paths";
 import HuntResult from "./HuntResult.view";
 import useHuntResultProps from "./useHuntResultProps";
 
@@ -6,6 +7,16 @@ const HuntResultPage: React.FC<void> = () => {
   const props = useHuntResultProps();
 
   return <HuntResult {...props} />;
+};
+
+export const getServerSideProps = () => {
+  // 바로 접근할 경우 hunt 페이지로 리다이렉트
+  return {
+    redirect: {
+      destination: ROUTES.HUNT,
+      permanent: false,
+    },
+  };
 };
 
 export default withBaseLayout(HuntResultPage);

1a8e01764921172977fc7ae7b37f4caed32adff6
Benjamin <eunvanz@naver.com>
2021-08-16 18:29:13 +0900

    test: withTestProvider 데코레이터 작성

---
+++ .storybook/decorators/withTestProvider.tsx
@@ -0,0 +1,11 @@
+import TestProvider from "../../components/TestProvider";
+
+const withTestProvider = (Story: any) => {
+  return (
+    <TestProvider>
+      <Story />
+    </TestProvider>
+  );
+};
+
+export default withTestProvider;
---
+++ components/TestProvider/index.ts
@@ -0,0 +1,5 @@
+import TestProvider from "./TestProvider";
+
+export type { TestProviderProps } from "./TestProvider";
+
+export default TestProvider;

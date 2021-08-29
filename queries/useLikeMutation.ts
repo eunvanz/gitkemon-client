8eb8ce1d54a28df5ff9e878fb25b7018495305ee
Benjamin <eunvanz@naver.com>
2021-08-29 11:33:38 +0900

    feat: add optimistic update logic to useLikeMutation

--- queries/useLikeMutation.ts
+++ queries/useLikeMutation.ts
@@ -1,8 +1,37 @@
-import { useMutation } from "react-query";
+import produce from "immer";
+import { useMutation, useQueryClient } from "react-query";
 import api from "~/api";
+import { assertNotEmpty } from "~/helpers/commonHelpers";
+import { Pageable, Painting, QUERY_KEY } from "~/types";
 
 const useLikeMutation = () => {
-  return useMutation(api.postLike);
+  const queryClient = useQueryClient();
+
+  return useMutation(api.postLike, {
+    onMutate: ({ contentId, contentType }) => {
+      switch (contentType) {
+        case "painting":
+          queryClient.setQueriesData<Pageable<Painting>>(
+            QUERY_KEY.PAINTING_LIST,
+            (oldData) => {
+              assertNotEmpty(oldData);
+              return {
+                ...oldData,
+                items: oldData.items.map((item) =>
+                  item.id !== contentId
+                    ? item
+                    : {
+                        ...item,
+                        likesCnt: item.likesCnt + 1,
+                      },
+                ),
+              };
+            },
+          );
+          break;
+      }
+    },
+  });
 };
 
 export default useLikeMutation;

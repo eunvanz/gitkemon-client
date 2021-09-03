7e3fa66c4b859f67682931cf87d8445ab78a1103
Benjamin <eunvanz@naver.com>
2021-09-03 17:51:47 +0900

    style: improve SelectButton's style

--- components/SelectButton/SelectButton.tsx
+++ components/SelectButton/SelectButton.tsx
@@ -13,9 +13,9 @@ const SelectButton: React.FC<SelectButtonProps<any>> = ({ value, onChange, items
           key={item.value}
           type="button"
           className={cx(
-            index === 0 ? "rounded-l-md border-r-0" : undefined,
+            index === 0 ? "rounded-l-md" : "-ml-px",
             index === items.length - 1 ? "rounded-r-md" : undefined,
-            index !== 0 && index !== items.length - 1 ? "border-r-0" : undefined,
+            index > 0 && value === items[index - 1].value ? "border-l-0" : undefined,
             value === item.value
               ? `bg-${colors.PRIMARY_COLOR} text-white border-${colors.PRIMARY_COLOR}`
               : "border-gray-300 bg-white text-gray-700 hover:bg-gray-50",

dbcea0571387d14ecf6a138f29687b007c789be8
Benjamin <eunvanz@naver.com>
2021-09-06 20:00:48 +0900

    feat: move sentry dsn

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,13 +1,14 @@
 // This file configures the initialization of Sentry on the browser.
 // The config you add here will be used whenever a page is visited.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://459ee4037cfb4ce2930ed5fcee88b7c0@o554586.ingest.sentry.io/5683357',
+  dsn:
+    SENTRY_DSN ||
+    "https://a5640d8620734642bec5562f3936b628@o554586.ingest.sentry.io/5947401",
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...

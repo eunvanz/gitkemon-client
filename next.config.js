8d5749e6565eaa8e841d845a22d6772f763d8b1e
Benjamin <eunvanz@naver.com>
2021-09-06 19:53:54 +0900

    feat: add sentry

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require("next/constants");
+const { withSentryConfig } = require("@sentry/nextjs");
 
 module.exports = (phase) => {
   // when started in development mode `next dev` or `npm run dev` regardless of the value of STAGING environmental variable
@@ -27,5 +28,17 @@ module.exports = (phase) => {
     pageExtensions: ["page.tsx", "page.ts"],
   };
 
-  return result;
+  const SentryWebpackPluginOptions = {
+    // Additional config options for the Sentry Webpack plugin. Keep in mind that
+    // the following options are set automatically, and overriding them is not
+    // recommended:
+    //   release, url, org, project, authToken, configFile, stripPrefix,
+    //   urlPrefix, include, ignore
+
+    silent: true, // Suppresses all logs
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options.
+  };
+
+  return withSentryConfig(result, SentryWebpackPluginOptions);
 };

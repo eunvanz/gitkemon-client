4fc0a87508f788cb1cf2a19de2958da856feae4e
Benjamin <eunvanz@naver.com>
2021-09-08 00:28:04 +0900

    feat: modify table style

--- api/requester.ts
+++ api/requester.ts
@@ -17,7 +17,7 @@ const instance = axios.create({
   baseURL: process.env.API_HOST,
 });
 instance.defaults.withCredentials = true;
-instance.defaults.timeout = 15_000;
+instance.defaults.timeout = 30_000;
 
 instance.interceptors.response.use(
   (response) => {
--- components/Table/Table.tsx
+++ components/Table/Table.tsx
@@ -27,7 +27,7 @@ const Table = <T extends object>({
     <div className="flex flex-col">
       <div className="-my-2 overflow-x-auto overflow-y-visible sm:overflow-x-visible sm:-mx-6 lg:-mx-8">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-          <div className="border border-gray-200 sm:rounded-lg">
+          <div className="border border-gray-200">
             <table className="min-w-full divide-y divide-gray-200">
               <thead className="bg-gray-50">
                 <tr>

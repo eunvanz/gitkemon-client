c962691cb0e614415ff7302cdd71f9e8c1ff1440
Benjamin <eunvanz@naver.com>
2021-09-14 23:18:01 +0900

    chore: add omiited change

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ module.exports = (phase) => {
     GITHUB_CLIENT_ID: isDev ? "1b594b7d4b0134060136" : "1b594b7d4b0134060136",
     API_HOST: isDev ? "http://localhost:3000" : "http://benjamin.ay1.krane.9rum.cc:8080",
     ACCESS_TOKEN_COOKIE_NAME: "gkmat",
+    GITHUB_HOST: "https://github.kakaocorp.com",
   };
 
   const result = {

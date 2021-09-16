e4094627cce58a00c4e05f39d79b275f6c13b639
Benjamin <eunvanz@naver.com>
2021-09-17 00:33:35 +0900

    feat: use locale name in showEvolutionConfirm

--- helpers/tsxHelpers.tsx
+++ helpers/tsxHelpers.tsx
@@ -2,18 +2,23 @@ import Dialog from "~/components/Dialog";
 import Typography from "~/components/Typography";
 import { Collection } from "~/types";
 import { capitalize } from "./commonHelpers";
+import { getLocaleProperty } from "./projectHelpers";
 
 export const showEvolutionConfirm = async (collection: Collection) => {
   const levelToDown = collection.level - collection.evolutionLevel!;
   const content =
     levelToDown === 0 ? (
       <>
-        <Typography color="primary">{capitalize(collection.name)}</Typography> will
-        disappear. Are you sure to evolve?
+        <Typography color="primary">
+          {capitalize(getLocaleProperty(collection, "name"))}
+        </Typography>{" "}
+        will disappear. Are you sure to evolve?
       </>
     ) : (
       <>
-        <Typography color="primary">{capitalize(collection.name)}</Typography>
+        <Typography color="primary">
+          {capitalize(getLocaleProperty(collection, "name"))}
+        </Typography>
         &apos;s level will be down to{" "}
         <Typography color="primary">{levelToDown}</Typography>. Are you sure to evolve?
       </>

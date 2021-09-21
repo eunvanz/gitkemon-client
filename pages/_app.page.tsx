5431f8f3710fc7e3e4736d867cd260fd43660cb7
Benjamin <eunvanz@naver.com>
2021-09-21 22:37:16 +0900

    feat: random meta image

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,4 +1,5 @@
 import { config } from "@fortawesome/fontawesome-svg-core";
+import { random } from "lodash";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { QueryClientProvider } from "react-query";
@@ -22,6 +23,24 @@ if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
   import("../mocks");
 }
 
+const images = [
+  "https://storage.googleapis.com/gitkemon.appspot.com/mon-images/mon_4_밥밥이_1628354906313.png",
+  "https://storage.googleapis.com/gitkemon.appspot.com/mon-images/mon_1_웅이_1628398611186.png",
+  "https://storage.googleapis.com/gitkemon.appspot.com/mon-images/mon_25_날지못하는새_1632043389835.png",
+  "https://storage.googleapis.com/gitkemon.appspot.com/mon-images/mon_91_밥밥이_1629640220468.png",
+  "https://storage.googleapis.com/gitkemon.appspot.com/mon-images/mon_172_련찌_1629298428532.png",
+  "https://storage.googleapis.com/gitkemon.appspot.com/mon-images/mon_209_anonymous_1630336218244.png",
+  "https://storage.googleapis.com/gitkemon.appspot.com/mon-images/mon_258_이론맛치즈쿠키_1630336031788.png",
+  "https://storage.googleapis.com/gitkemon.appspot.com/mon-images/mon_674_나무판_1630337529596.png",
+  "https://storage.googleapis.com/gitkemon.appspot.com/mon-images/painting_94_이론맛치즈쿠키_1630930675273.png",
+  "https://storage.googleapis.com/gitkemon.appspot.com/mon-images/mon_133_웅이_1629455901824.png",
+  "https://storage.googleapis.com/gitkemon.appspot.com/mon-images/mon_130_레옹엄마_1629455468665.png",
+  "https://storage.googleapis.com/gitkemon.appspot.com/mon-images/mon_557_나랏님말씀_1630336392146.png",
+  "https://storage.googleapis.com/gitkemon.appspot.com/mon-images/mon_621_로코_1630337176315.png",
+];
+
+const image = images[random(0, images.length - 1)];
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
@@ -38,18 +57,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           property="og:description"
           content="Get Pokémons as payback for Github contributions"
         />
-        <meta
-          property="og:image"
-          content="https://storage.googleapis.com/gitkemon.appspot.com/mon-images/mon_4_밥밥이_1628354906313.png"
-        />
-        <meta
-          property="og:image:url"
-          content="https://storage.googleapis.com/gitkemon.appspot.com/mon-images/mon_4_밥밥이_1628354906313.png"
-        />
-        <meta
-          property="og:image:secure_url"
-          content="https://storage.googleapis.com/gitkemon.appspot.com/mon-images/mon_4_밥밥이_1628354906313.png"
-        />
+        <meta property="og:image" content={image} />
+        <meta property="og:image:url" content={image} />
+        <meta property="og:image:secure_url" content={image} />
         <meta property="og:image:type" content="image/png" />
         <meta property="og:image:width" content="250" />
         <meta property="og:image:height" content="250" />

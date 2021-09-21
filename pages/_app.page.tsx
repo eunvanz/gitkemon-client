c69c07c17e6826bddb3e607ba164638ad7407905
Benjamin <eunvanz@naver.com>
2021-09-21 18:43:27 +0900

    feat: move meta tags to _app

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,5 +1,6 @@
 import { config } from "@fortawesome/fontawesome-svg-core";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { ToastContainer } from "react-toastify";
@@ -23,18 +24,50 @@ if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <ToastContainer
-          position="bottom-right"
-          newestOnTop
-          progressStyle={{ backgroundColor: colorHashes.WATER }}
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,user-scalable=no"
         />
-        <TopProgressBarContainer />
-        <Component {...pageProps} />
-      </RecoilRoot>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+        <meta charSet="utf-8" />
+        <meta property="og:url" content="https://gitkemon.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Gitkémon" />
+        <meta
+          property="og:description"
+          content="Get Pokémons as payback for Github contributions"
+        />
+        <meta
+          property="og:image"
+          content="https://storage.googleapis.com/gitkemon.appspot.com/mon-images/mon_4_밥밥이_1628354906313.png"
+        />
+        <meta
+          property="og:image:url"
+          content="https://storage.googleapis.com/gitkemon.appspot.com/mon-images/mon_4_밥밥이_1628354906313.png"
+        />
+        <meta
+          property="og:image:secure_url"
+          content="https://storage.googleapis.com/gitkemon.appspot.com/mon-images/mon_4_밥밥이_1628354906313.png"
+        />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="250" />
+        <meta property="og:image:height" content="250" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <ToastContainer
+            position="bottom-right"
+            newestOnTop
+            progressStyle={{ backgroundColor: colorHashes.WATER }}
+          />
+          <TopProgressBarContainer />
+          <Component {...pageProps} />
+        </RecoilRoot>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </>
   );
 }
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,35 +9,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en-US">
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width,initial-scale=1,user-scalable=no"
-          />
-          <meta charSet="utf-8" />
-          <meta property="og:url" content="https://gitkemon.com" />
-          <meta property="og:type" content="website" />
-          <meta
-            property="og:description"
-            content="Get Pokémons as payback for Github contributions"
-          />
-          <meta
-            property="og:image"
-            content="https://storage.googleapis.com/gitkemon.appspot.com/mon-images/mon_4_밥밥이_1628354906313.png"
-          />
-          <meta
-            property="og:image:url"
-            content="https://storage.googleapis.com/gitkemon.appspot.com/mon-images/mon_4_밥밥이_1628354906313.png"
-          />
-          <meta
-            property="og:image:secure_url"
-            content="https://storage.googleapis.com/gitkemon.appspot.com/mon-images/mon_4_밥밥이_1628354906313.png"
-          />
-          <meta property="og:image:type" content="image/png" />
-          <meta property="og:image:width" content="250" />
-          <meta property="og:image:height" content="250" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />

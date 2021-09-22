1e1f7f9249b7dd1d3598874ea1f5abdda44a07fd
Benjamin <eunvanz@naver.com>
2021-09-22 11:23:13 +0900

    feat: redirect to home when signed in in sign in view

--- pages/sign-in/useSignInProps.ts
+++ pages/sign-in/useSignInProps.ts
@@ -1,11 +1,25 @@
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
+import { useRouter } from "next/router";
+import { useRecoilValue } from "recoil";
 import { signInWithGithub } from "~/helpers/projectHelpers";
+import ROUTES from "~/paths";
+import { userState } from "~/state/user";
 import { SignInProps } from "./SignIn.view";
 
 const useSignInProps: () => SignInProps = () => {
   const onSignIn = useCallback(() => {
     signInWithGithub();
   }, []);
+
+  const user = useRecoilValue(userState);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      router.replace(ROUTES.HOME);
+    }
+  }, [router, user]);
 
   return {
     onSignIn,
